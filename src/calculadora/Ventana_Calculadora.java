30daca2753153de76dbdd271a3698b23e4e2fe49
Exequiel Raineri <exeraineriuni@gmail.com>
2022-11-25 18:32:22 -0300

    Primera impresion de la calculadora

--- src/calculadora/Ventana_Calculadora.java
+++ src/calculadora/Ventana_Calculadora.java
@@ -509,6 +509,9 @@ public class Ventana_Calculadora extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(Ventana_Calculadora.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
